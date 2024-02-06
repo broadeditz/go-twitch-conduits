701a5b0fa90dc20dd71a940ea5cd3088a5393f4b
bread <75878656+broadeditz@users.noreply.github.com>
2024-02-06 19:46:47 +0100

    patch conduit shards

--- pkg/conduit/conduit.go
+++ pkg/conduit/conduit.go
@@ -19,7 +19,6 @@ type CreateConduitResponse struct {
 // CreateConduit sends a request to the Twitch API to create a conduit with the given shard count
 func (t *TwitchAPI) CreateConduit(shardCount int) (*CreateConduitResponse, error) {
 	// POST to 'https://api.twitch.tv/helix/eventsub/conduits' with authorization & client ID headers, shard count in body
-
 	requestData, body := map[string]interface{}{
 		"shard_count": shardCount,
 	}, new(bytes.Buffer)
@@ -40,6 +39,54 @@ func (t *TwitchAPI) CreateConduit(shardCount int) (*CreateConduitResponse, error
 	}
 
 	var response CreateConduitResponse
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+type AssignConduitTransportRequest struct {
+	ConduitID string           `json:"conduit_id"`
+	Shards    []TransportShard `json:"shards"`
+}
+
+// TransportShard is a shard of a conduit
+type TransportShard struct {
+	ID        string `json:"id"`
+	Status    string `json:"status,omitempty"`
+	Transport struct {
+		Method   string `json:"method"`
+		Callback string `json:"callback"`
+		Secret   string `json:"secret"`
+	}
+}
+
+type AssignConduitTransportResponse struct {
+	Data []TransportShard `json:"data"`
+}
+
+// AssignConduitTransport sends a request to the Twitch API to assign a transport to shards of a conduit
+func (t *TwitchAPI) AssignConduitTransport(request AssignConduitTransportRequest) (*AssignConduitTransportResponse, error) {
+	// PATCH to 'https://api.twitch.tv/helix/eventsub/conduits/shards' with authorization & client ID headers, request body
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(request)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := t.Do(http.MethodPatch, "https://api.twitch.tv/helix/eventsub/conduits/shards", body)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("Assign Conduit Transport unexpected status code: %d", res.StatusCode))
+	}
+
+	var response AssignConduitTransportResponse
 	err = json.NewDecoder(res.Body).Decode(&response)
 	if err != nil {
 		return nil, err

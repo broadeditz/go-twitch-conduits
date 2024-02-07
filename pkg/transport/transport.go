157b60d8b382435b61d1e24b690cba6b79061de7
bread <75878656+broadeditz@users.noreply.github.com>
2024-02-07 15:58:37 +0100

    add Start to Transport interface

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -2,5 +2,7 @@ package transport
 
 // Transport is used to manage websocket & webhook conduits as a single interface
 type Transport interface {
+	Start() error
 	OnChannelMessage(func(message ChannelMessage))
+	// TODO: Add more event types
 }

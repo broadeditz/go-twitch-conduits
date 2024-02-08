0954a73a802961d1db6e0df4b5e852e19620a61b
bread <75878656+broadeditz@users.noreply.github.com>
2024-02-08 19:17:50 +0100

    change transport interface definition

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -2,7 +2,8 @@ package transport
 
 // Transport is used to manage websocket & webhook conduits as a single interface
 type Transport interface {
-	Start() error
+	Init() error
+	Close()
 	OnChannelMessage(func(message ChannelMessage))
 	// TODO: Add more event types
 }

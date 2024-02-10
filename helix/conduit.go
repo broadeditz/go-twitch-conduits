bf0fee079342ea637dbd6119024e0878247c4afd
bread <75878656+broadeditz@users.noreply.github.com>
2024-02-10 20:54:14 +0100

    Add GetConduits

--- helix/conduit.go
+++ helix/conduit.go
@@ -8,7 +8,29 @@ import (
 	"net/http"
 )
 
+// GetConduits returns a list of currently active conduits for the twitch app
+func (t *TwitchAPI) GetConduits() (*CreateConduitResponse, error) {
+	// GET to 'https://api.twitch.tv/helix/eventsub/conduits'
+	res, err := t.Do(http.MethodGet, "https://api.twitch.tv/helix/eventsub/conduits", nil)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("Get Conduits unexpected status code: %d, %+v", res.StatusCode, res.Status))
+	}
+
+	var response CreateConduitResponse
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
 // CreateConduitResponse is the response the Twitch API returns when creating a conduit
+// TODO: for v1.0.0 rename this to make more sense since the same response also comes from GetConduits
 type CreateConduitResponse struct {
 	Data []struct {
 		ID         string `json:"id"`
@@ -46,6 +68,20 @@ func (t *TwitchAPI) CreateConduit(shardCount int) (*CreateConduitResponse, error
 	}
 
 	return &response, nil
+}
+
+func (t *TwitchAPI) DeleteConduit(conduitID string) error {
+	res, err := t.Do(http.MethodDelete, fmt.Sprintf("https://api.twitch.tv/helix/eventsub/conduits?id=%v", conduitID), nil)
+	defer res.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusNoContent {
+		return errors.New(fmt.Sprintf("Delete Conduit unexpected status code: %d, %+v", res.StatusCode, res.Status))
+	}
+
+	return nil
 }
 
 // AssignConduitTransportRequest is the request body for assigning a transport to shards of a conduit
